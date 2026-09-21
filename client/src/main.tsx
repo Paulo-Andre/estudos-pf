0706a421502e1224da70a5662d0f08f081e8564c
Paulo-Andre <paulo.abd@hotmail.com>
2026-09-21 09:55:52 -0300

    feat: register safe static-only service worker

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -30,3 +30,10 @@ createRoot(document.getElementById("root")!).render(
     </QueryClientProvider>
   </trpc.Provider>
 );
+
+
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => undefined);
+  });
+}
