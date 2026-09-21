398901fa6648b6759b060a7d3e4f752f295f6d36
Paulo-Andre <paulo.abd@hotmail.com>
2026-09-21 09:59:29 -0300

    feat: expose Django course administration endpoints

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -1,3 +1,14 @@
 from django.urls import path
-from .views import AccessView,ContentView,CourseListView
-urlpatterns=[path("",CourseListView.as_view()),path("access/",AccessView.as_view()),path("<str:course_id>/content/",ContentView.as_view())]
+from .views import AccessView,AdminCourseActiveView,AdminCourseDetailView,AdminCoursesView,AdminEnrollmentListView,AdminGrantEnrollmentView,AdminRevokeEnrollmentView,ContentView,CourseListView
+
+urlpatterns=[
+    path("",CourseListView.as_view()),
+    path("access/",AccessView.as_view()),
+    path("<str:course_id>/content/",ContentView.as_view()),
+    path("admin/",AdminCoursesView.as_view()),
+    path("admin/<str:course_id>/",AdminCourseDetailView.as_view()),
+    path("admin/<str:course_id>/active/",AdminCourseActiveView.as_view()),
+    path("admin/users/<int:user_id>/enrollments/",AdminEnrollmentListView.as_view()),
+    path("admin/enrollments/grant/",AdminGrantEnrollmentView.as_view()),
+    path("admin/enrollments/revoke/",AdminRevokeEnrollmentView.as_view()),
+]
