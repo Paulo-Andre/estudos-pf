01a06754ad7134ae7d2bf37fbb162bc8dbf200c1
Paulo-Andre <paulo.abd@hotmail.com>
2026-09-22 09:51:23 -0300

    Registra versões de edital no admin Django

--- backend/study/admin.py
+++ backend/study/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import StudyBookmark,CompletedModule,SimulationRecord,SimulationReflection,StudyAnswer,StudyContentProgress,StudyNote,StudyProfile,StudyReviewItem,StudyRoadmapItem
-for m in [StudyProfile,CompletedModule,StudyAnswer,SimulationRecord,SimulationReflection,StudyNote,StudyReviewItem,StudyContentProgress,StudyRoadmapItem]:
+from .models import StudyBookmark,CompletedModule,SimulationRecord,SimulationReflection,StudyAnswer,StudyContentProgress,StudyNote,StudyProfile,StudyReviewItem,StudyRoadmapItem,StudySyllabusSnapshot
+for m in [StudyProfile,CompletedModule,StudyAnswer,SimulationRecord,SimulationReflection,StudyNote,StudyReviewItem,StudyContentProgress,StudyRoadmapItem,StudySyllabusSnapshot]:
     admin.site.register(m)
 
 admin.site.register(StudyBookmark)
