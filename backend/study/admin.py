314db66cd39c79b24ab51e9524353220d1a02645
Paulo-Andre <paulo.abd@hotmail.com>
2026-09-22 10:21:29 -0300

    Registra configurações de inteligência no admin Django

--- backend/study/admin.py
+++ backend/study/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import StudyBookmark,CompletedModule,SimulationRecord,SimulationReflection,StudyAnswer,StudyContentProgress,StudyNote,StudyProfile,StudyReviewItem,StudyRoadmapItem,StudySyllabusSnapshot
-for m in [StudyProfile,CompletedModule,StudyAnswer,SimulationRecord,SimulationReflection,StudyNote,StudyReviewItem,StudyContentProgress,StudyRoadmapItem,StudySyllabusSnapshot]:
+from .models import StudyBookmark,CompletedModule,SimulationRecord,SimulationReflection,StudyAnswer,StudyContentProgress,StudyNote,StudyProfile,StudyReviewItem,StudyRoadmapItem,StudySyllabusSnapshot,LearningIntelligenceSettings
+for m in [StudyProfile,CompletedModule,StudyAnswer,SimulationRecord,SimulationReflection,StudyNote,StudyReviewItem,StudyContentProgress,StudyRoadmapItem,StudySyllabusSnapshot,LearningIntelligenceSettings]:
     admin.site.register(m)
 
 admin.site.register(StudyBookmark)
